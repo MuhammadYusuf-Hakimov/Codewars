5e436ec2ccc8b1c160c68b29d597d8f416d4996f
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-08-04 17:10:10 +0500

    Codewars

---
+++ day1/index.js
@@ -0,0 +1,10 @@
+// function solution(a, b) {
+//   return a.length < b.length ? a + b + a : b + a + b;
+// }
+
+// function fakeBin(x) {
+//   return x
+//     .split("")
+//     .map(digit => digit < 5 ? "0" : "1")
+//     .join("");
+// }
