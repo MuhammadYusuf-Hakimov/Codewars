a569354507b3b6d382079a1d35a2a93953ac2f6f
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-08-06 13:01:34 +0500

    2-kun

---
+++ day2/index.js
@@ -0,0 +1,8 @@
+// function well(x) {
+//   const n = x.filter(v => v === "good").length;
+//   return n === 0 ? "Fail!" : n <= 2 ? "Publish!" : "I smell a series!";
+// }
+
+// function stringToNumber(str) {
+//   return Number(str);
+// }
---
+++ day3/index.js
@@ -0,0 +1,12 @@
+// function toCsvText(array) {
+//   return array.map(row => row.join(",")).join("\n");
+// }
+// function countSheep(num) {
+//   let result = "";
+  
+//   for (let i = 1; i <= num; i++) {
+//     result += `${i} sheep...`;
+//   }
+  
+//   return result;
+// }
