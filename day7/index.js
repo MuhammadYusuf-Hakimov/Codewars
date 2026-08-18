2ecf022e530ca149c5784e931e5f6e352e198c0b
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-08-18 13:21:59 +0500

    7-kun

---
+++ day7/index.js
@@ -0,0 +1,8 @@
+// function removeChar(str) {
+//   return str.slice(1, -1);
+// }
+
+// function feast(beast, dish) {
+//   return beast[0] === dish[0] &&
+//          beast.at(-1) === dish.at(-1);
+// }
