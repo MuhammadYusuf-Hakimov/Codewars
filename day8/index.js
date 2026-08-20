ebfa4a5aaea3294ed99ef335b67f228bdef25085
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-08-20 12:56:08 +0500

    Vazifa

---
+++ day8/index.js
@@ -0,0 +1,11 @@
+// function greet(name, owner) {
+//   if (name === owner) {
+//     return 'Hello boss';
+//   } else {
+//     return 'Hello guest';
+//   }
+// }
+
+// function makeUpperCase(str) {
+//   return str.toUpperCase();
+// }
