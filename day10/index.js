c161a203ee9142cbb01fc91dbe83c1904680b86a
MuhammadYusuf-Hakimov <muhammadyusuf201277711@gmail.com>
2026-08-22 13:46:13 +0500

    code

---
+++ day10/index.js
@@ -0,0 +1,21 @@
+// function mouthSize(animal) {
+//     return animal.toLowerCase() === "alligator" ? "small" : "wide";
+// }
+
+// function brightest(colors) {
+//     return colors.reduce((a, b) => {
+//         let av = Math.max(
+//             parseInt(a.slice(1, 3), 16),
+//             parseInt(a.slice(3, 5), 16),
+//             parseInt(a.slice(5, 7), 16)
+//         );
+
+//         let bv = Math.max(
+//             parseInt(b.slice(1, 3), 16),
+//             parseInt(b.slice(3, 5), 16),
+//             parseInt(b.slice(5, 7), 16)
+//         );
+
+//         return bv > av ? b : a;
+//     });
+// }
